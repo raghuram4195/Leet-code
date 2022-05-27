83f68830ac447ecd9525e2832fa20889a345b251
raghuram4195 <61089253+raghuram4195@users.noreply.github.com>
2022-05-26 21:22:38 -0600

    Time: 71 ms, Memory: 43.2 MB - LeetHub

---
+++ power-of-three/power-of-three.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        if(n==0)
+            return false;
+        int temp = n % 10;
+        if(temp == 1 || temp == 3 || temp == 9 || temp == 7)
+        {
+            while(n!=0)
+            {
+                                if(n==1)
+                    return true;
+                System.out.println(n);
+                if(n%3!=0)
+                    return false;
+                n = n /3;
+
+            }
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+       
+    }
+}
