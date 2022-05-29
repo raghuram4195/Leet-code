e53ee1d2d5892db811e5ed4acdec7eb997872cad
raghuram4195 <61089253+raghuram4195@users.noreply.github.com>
2022-05-28 19:28:50 -0600

    Time: 1 ms, Memory: 51.7 MB - LeetHub

---
+++ missing-number/missing-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        boolean[] arr = new boolean[nums.length+1];
+        for(int i=0;i<nums.length;i++)
+        {
+            arr[nums[i]]=true;
+        }
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]!=true)
+                return i;
+        }
+        return 1;
+    }
+}
