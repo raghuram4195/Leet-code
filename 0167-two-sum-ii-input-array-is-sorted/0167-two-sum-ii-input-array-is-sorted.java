0a53ef5b78819ee75d626d0ca469da05b3c65a4f
raghuram4195 <61089253+raghuram4195@users.noreply.github.com>
2022-10-29 01:43:27 -0600

    Time: 10 ms (26.62%), Space: 49.9 MB (58.66%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -4,20 +4,37 @@ class Solution {
        
         
         int[] res = new int[2];
-       for(i=0;i<nums.length-1;i++)
-        {
-           for(int j=i+1;j<nums.length;j++)
-           {
-               if(nums[i]+nums[j]==target)
-               {
-                   res[0]=i+1;
-                   res[1] = j+1;
-                   return res;
-               }
-               if(nums[i]+nums[j]>target)
-                   break;
-           }
+//        for(i=0;i<nums.length-1;i++)
+//         {
+//            for(int j=i+1;j<nums.length;j++)
+//            {
+//                if(nums[i]+nums[j]==target)
+//                {
+//                    res[0]=i+1;
+//                    res[1] = j+1;
+//                    return res;
+//                }
+//                if(nums[i]+nums[j]>target)
+//                    break;
+//            }
             
+//         }
+        int left = 0;
+        int right = nums.length - 1;
+        while(left<right){
+            int target2= nums[left] + nums[right];
+            if(target==target2)
+            {
+                res[0]=left+1;
+                res[1] = right+1;
+                return res;
+            }
+            else if(target>target2)
+            {
+                left++;
+            }
+            else
+                right--;
         }
         return res;
     }
